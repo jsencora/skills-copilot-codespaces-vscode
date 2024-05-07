cd28a16a6d5baa06af8a764aad6fa5ee436c58cc
jsencora <168609340+jsencora@users.noreply.github.com>
2024-05-07 02:50:08 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+  var member = {
+    name: 'John Doe',
+    age: 25,
+    skills: ['JS', 'HTML', 'CSS'],
+    address: {
+      city: 'New York',
+      country: 'USA'
+    }
+  }
+  return member;
+}
